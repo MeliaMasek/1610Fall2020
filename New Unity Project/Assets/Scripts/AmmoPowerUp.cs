64654d7206e495dc86a2c7a203938d03902e14e6
Melia <10910291@my.uvu.edu>
2020-09-11 21:48:27 -0600

    Added AmmoPowerUp

---
+++ New Unity Project/Assets/Scripts/AmmoPowerUp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AmmoPowerUp : MonoBehaviour
+{
+    public int ammo;
+    private void OnTriggerEnter(Collider other)
+    {
+        ammo++;
+        print(ammo);
+    }
+}
