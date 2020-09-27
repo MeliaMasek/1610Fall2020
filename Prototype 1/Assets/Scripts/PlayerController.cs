6621ee41432d18b93fecf8baa3e018212e0335f9
Melia <10910291@my.uvu.edu>
2020-09-27 00:53:23 -0600

    Renamed unity files, updated 1st create with code script

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public float speed = 20f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+    
+    void Update()
+    { 
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+       
+        // rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        
+       //Moves the car forward based on vertical input 
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
+    }
+}
