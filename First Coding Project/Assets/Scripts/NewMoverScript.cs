dcec6b4577603bd84bf20b4a5d6f1a9d4f7c93da
Melia <10910291@my.uvu.edu>
2020-10-02 14:57:21 -0600

    updated project from class

---
+++ First Coding Project/Assets/Scripts/NewMoverScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMoverScript : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.x = moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            moveDirection.x = -moveSpeed * Time.deltaTime;
+        }
+        transform.Translate(moveDirection);
+    }
+}
---
+++ First Coding Project/Assets/Scripts/UpdatedMoverScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatedMoverScript : MonoBehaviour
+{
+    public float speed = 10f;
+    public Vector3 rotationVector3;
+    public string password;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (password == "OU812" )
+        {
+            print("Password Correct");
+        }
+        rotationVector3.y = speed * Time.deltaTime;
+        transform.Rotate(rotationVector3);
+    }
+}
