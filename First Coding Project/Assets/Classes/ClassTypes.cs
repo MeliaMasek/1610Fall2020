7ffd64fe99bcf2579c3e4722d791256ab57fba9a
Melia <10910291@my.uvu.edu>
2020-10-28 14:05:00 -0600

    Write a new scriptable object with variables of game data

---
+++ First Coding Project/Assets/Classes/ClassTypes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassTypes : MonoBehaviour
+{
+    public List<Classes> classList;
+    private void Start()
+    {
+        for (var i = 0; i < classList.Count; i++)
+        {
+            var position = new Vector3(i*2, 0, 0);
+            var item = classList[i];
+            var newItem = new GameObject(item.name);
+            var sprite = newItem.AddComponent<SpriteRenderer>();
+         
+            newItem.transform.position = position;
+         
+            sprite.sprite = item.art;
+            sprite.color = item.color;
+        }
+    }
+}
---
+++ First Coding Project/Assets/Classes/Classes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Classes : ScriptableObject
+{
+//    public List<Classes> ClassList;
+    public string type;
+    public float health;
+    public Sprite art;
+    public Color color;
+}
