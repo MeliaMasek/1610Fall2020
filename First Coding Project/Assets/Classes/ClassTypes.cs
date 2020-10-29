e83e02708f8a2f8dc6f65ed24b08bc69e4710c66
Melia <10910291@my.uvu.edu>
2020-10-29 10:44:56 -0600

    Organized Layout

--- First Coding Project/Assets/Classes/ClassTypes.cs
+++ First Coding Project/Assets/Classes/ClassTypes.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class ClassTypes : MonoBehaviour
 {
     public List<Classes> classList;
+
     private void Start()
     {
         for (var i = 0; i < classList.Count; i++)
         {
-            var position = new Vector3(i*2, 0, 0);
+            var position = new Vector3(i * 2, 0, 0);
             var item = classList[i];
             var newItem = new GameObject(item.name);
             var sprite = newItem.AddComponent<SpriteRenderer>();
-         
+
             newItem.transform.position = position;
-         
+
             sprite.sprite = item.art;
             sprite.color = item.color;
+        }
+
+        foreach (var item in classList)
+        {
+            
         }
     }
 }
