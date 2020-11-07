83ed9da34a35a23aa8a2024c2290513e16239f1e
Melia <10910291@my.uvu.edu>
2020-11-06 20:53:20 -0700

    Write an Event

--- First Coding Project/Assets/Scripts/UnityEvents.cs
+++ First Coding Project/Assets/Scripts/UnityEvents.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 using UnityEngine.Events;
 public class UnityEvents : MonoBehaviour
 {
-    public UnityEvent myUnityEvent;
+   public UnityEvent triggerExitEvent;
+   public UnityEvents triggerStayEvent;
+   public UnityEvents awakeEvent; 
+   
+   public UnityEvent myUnityEvent;
 
     private void OnTriggerEnter(Collider other)
     {
-        myUnityEvent.Invoke();
+       myUnityEvent.Invoke();
     }
+   private void OnTriggerExit(Collider other)
+   {
+      triggerExitEvent.Invoke();
+   }
+
+   private void OnTriggerStay(Collider other)
+   {
+      triggerStayEvent.Invoke(null, 0);
+   }
+   private void Awake()
+   {
+      awakeEvent.Invoke(null, 0);
+   }
 }
