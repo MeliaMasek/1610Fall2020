593161519837734da8998ecd19d50cdea38323da
Melia <10910291@my.uvu.edu>
2020-11-10 22:13:03 -0700

    class work 11/10

---
+++ First Coding Project/Assets/ScriptableObjects/DefenseSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class DefenseSwitch : ScriptableObject
+{
+  public enum Defences
+  {
+    Shield,
+    Potions,
+    ExtraHealth,
+    Buffs,
+    SuperShield,
+    Armor,
+    SuperArmor,
+  }
+
+  public Defences currentDefence;
+
+  public void RunCurrentDefence()
+  {
+    switch (currentDefence)
+    {
+      case Defences.Shield:
+        break;
+      case Defences.Potions:
+        break;
+      case Defences.ExtraHealth:
+        break;
+      case Defences.Buffs:
+        break;
+      case Defences.SuperShield:
+        break;
+      case Defences.Armor:
+        break;
+      case Defences.SuperArmor:
+        break;
+      default:
+        throw new ArgumentOutOfRangeException();
+    }
+  }
+}
---
+++ First Coding Project/Assets/Scripts/Defense.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Defense : Collectible
+{
+    public float PowerLevel = .5f;
+
+    public void Use()
+    {
+     Debug.Log(this.name);   
+    }
+}
