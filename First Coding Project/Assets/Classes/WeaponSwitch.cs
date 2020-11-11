bf12c9d3b3b9ea2106096ba77464a3b3f1c7d0f7
Melia <10910291@my.uvu.edu>
2020-11-10 22:21:33 -0700

    Script a weapons switch

---
+++ First Coding Project/Assets/Classes/WeaponSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class WeaponSwitch : ScriptableObject
+{
+public enum WeaponClasses
+{
+    Axe,
+    Hammer, 
+    Spear,
+    Bow,
+    Sword,
+}
+
+public WeaponClasses currentWeaponClasses = WeaponClasses.Sword;
+
+public void ChangeToBow()
+{
+    currentWeaponClasses = WeaponClasses.Bow;
+}
+
+public void RunCurrentState()
+    {
+    switch (currentWeaponClasses)
+        {
+        case WeaponClasses.Axe:
+            // equips Axe    
+            break;
+        case WeaponClasses.Hammer: 
+            // equips Hammer 
+            break;
+        case WeaponClasses.Spear:
+            // equips Spear 
+            break;
+        case WeaponClasses.Bow:
+            // equips Bow 
+            break;
+        case WeaponClasses.Sword:
+            // equips Sword
+            break;
+        default:
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
