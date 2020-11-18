e77979ad9a50b382ad9be10a8c3d5050eb222abb
Melia <10910291@my.uvu.edu>
2020-11-18 12:38:34 -0700

    Updated Script an alternative AI task

---
+++ First Coding Project/Assets/Scripts/WatchPoints.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class WatchPoints : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform destination;
+    private int i = 0;
+    private bool canWatch = true;
+    public List<Transform> patrolPoints;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination = transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canWatch = false;
+        destination = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canWatch = true;
+    }
+
+    private void Update()
+    {
+        agent.destination = destination.position;
+        if (!canWatch) return;
+
+        if (agent.pathPending && agent.remainingDistance<0.5f)
+        {
+            destination = patrolPoints[1];
+            i = (i + 1) % patrolPoints.Count;
+        }
+    }
+}
