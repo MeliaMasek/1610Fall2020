ec9b448d5a83bf386f0e8950459f19efb7af0b0d
Melia <10910291@my.uvu.edu>
2020-11-20 00:23:57 -0700

    Messing around with co-routines

---
+++ First Coding Project/Assets/Scripts/CountDown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+public class CountDown : MonoBehaviour
+{
+    public UnityEvent startEvent, endEvent;
+    public float holdTime = 1f; //repeatHoldTime = .25f;
+    public int counter = 3;
+    public bool ison;
+    public void RunCoroutine()
+    {
+        StartCoroutine(Coroutine());
+    }
+    
+    private IEnumerator Coroutine()
+    {
+        startEvent.Invoke();
+        yield return new WaitForSeconds(holdTime);
+
+        //while (counter > 0)
+        {
+            //repeatEvent.Invoke();
+            //yield return new WaitForSeconds(repeatHoldTime);
+            ////counter--;
+        }
+        yield return new WaitForSeconds(holdTime);
+        endEvent.Invoke();
+    }
+}
