b35cfded06a898a1c9cca7da4e2b28b8e262bf9b
Melia <10910291@my.uvu.edu>
2020-11-19 22:24:46 -0700

    Class Work 11/19

---
+++ First Coding Project/Assets/Scripts/AddForce.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class AddForce : MonoBehaviour
+{
+    private Rigidbody rigid;
+    public Vector3 forces, forces2;
+    public float holdTime = 3f;
+    public int counter = 10;
+    public bool canRun;
+    private IEnumerable Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(holdTime);
+            rigid.AddForce(forces);
+            counter--;
+        }
+
+        while (canRun)
+        {
+            yield return new WaitForSeconds(holdTime);
+            rigid.AddForce(forces2);
+        }
+    }
+}
