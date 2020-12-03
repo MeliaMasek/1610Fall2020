ca4dd12fecee1e35aee0d0346aabdb029b4de9c3
Melia <10910291@my.uvu.edu>
2020-12-02 23:25:57 -0700

    Updated counter script homework

---
+++ First Coding Project/Assets/Scripts/NumberCountdown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class NumberCountdown : MonoBehaviour
+{
+    private Text textobj;
+    public float delay = 1f;
+    public UnityEvent goEvent;
+    IEnumerator Start()
+    {
+        textobj = GetComponent<Text>();
+        textobj.text = "3";
+        var i = 3;
+        while (i > 0)
+        {
+            textobj.text = i.ToString();
+            yield return new WaitForSeconds(delay);
+            i--;
+        }
+
+        textobj.text = "Go!";
+        yield return new WaitForSeconds(delay);
+        textobj.text = " ";
+        goEvent.Invoke();
+    }
+}
