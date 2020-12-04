60ae98bb6153ff36a9522a3630e66d005de89d09
Melia <10910291@my.uvu.edu>
2020-12-04 12:53:53 -0700

    Animation Controllers

---
+++ First Coding Project/Assets/Scripts/JumpInputAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class JumpInputAnimation : MonoBehaviour
+{
+    private Animator anims;
+
+    private void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+
+        {
+            anims.SetTrigger("Jump");
+        }
+    }
+}
