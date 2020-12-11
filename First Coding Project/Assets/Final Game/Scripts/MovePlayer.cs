8b5c07e356480801687761b8f652ce2bccf95ef5
Melia <10910291@my.uvu.edu>
2020-12-10 21:22:24 -0700

    Final game work

---
+++ First Coding Project/Assets/Final Game/Scripts/MovePlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class MovePlayer : MonoBehaviour
+{
+    private CharacterController controller;
+    public float moveSpeed = 3f, gravity = -9.81f, jumpForce = 5f;
+    
+    private Vector3 moveDirection;
+    private float yDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal");
+        moveDirection.Set(moveSpeedInput,yDirection,0);       
+        yDirection += gravity*Time.deltaTime;
+
+        if (controller.isGrounded && moveDirection.y < 0)
+        {
+            yDirection = -1f;
+        }
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            yDirection = jumpForce;
+        }
+        controller.Move(moveDirection*Time.deltaTime);
+    }
+}
